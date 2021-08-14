7991fa2074f4522eef43379964e04ae16965f2be
U-ELDHRIMNIR\eigen <11557146+p3r7@users.noreply.github.com>
2021-08-14 11:15:35 +0200

    actual code

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # gridkeys
-mod for monome norns, use grid as midi keyboard anywhere
+
+mod for monome norns, use grid as midi keyboard anywhere.
+
+notes will be sent to the input of the `virtual` midi device iff current script is listening for it (as bound an `event` function to it).
+
+if a script uses grid, this mod won't take effect.
+
+right now only molly-style layout is supported, something like gridstep's would be more versatile.
---
+++ lib/mod.lua
@@ -0,0 +1,67 @@
+local mod = require 'core/mods'
+
+local state = {
+  grid_device = nil,
+  midi_in_devices = {},
+}
+
+local function midi_msg_to_virtual(msg)
+  local data = midi.to_data(msg)
+  for _, dev in pairs(midi.devices) do
+    if dev.port ~= nil and dev.name == 'virtual' then
+      if midi.vports[dev.port].event ~= nil then -- current script listen for this device
+        midi.vports[dev.port].event(data)
+        return true
+      end
+    end
+  end
+  return false
+end
+
+local function note_on(note_num, vel)
+  local msg = {
+    type = 'note_on',
+    note = note_num,
+    vel = vel,
+    ch = 1,
+  }
+  return midi_msg_to_virtual(msg)
+end
+
+local function note_off(note_num)
+  local msg = {
+    type = 'note_off',
+    note = note_num,
+    vel = 100,
+    ch = 1,
+  }
+  return midi_msg_to_virtual(msg)
+end
+
+local function grid_key(x, y, z)
+  local note_num = util.clamp(((7 - y) * 5) + x + 33, 0, 127)
+
+  local midi_d_is_active = false
+  if z == 1 then
+    midi_d_is_active = note_on(note_num, 100)
+    if midi_d_is_active then
+      state.grid_device:led(x, y, 15)
+    end
+  else
+    midi_d_is_active = note_off(note_num)
+    if midi_d_is_active then
+      state.grid_device:led(x, y, 0)
+    end
+  end
+
+  if midi_d_is_active then
+    state.grid_device:refresh()
+  end
+end
+
+
+
+mod.hook.register("script_pre_init", "gridkeys", function()
+                    state.grid_device = grid.connect(1)
+                    state.grid_device.key = grid_key
+end)
