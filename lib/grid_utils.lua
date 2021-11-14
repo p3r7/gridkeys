9409ad33735651faabb64ecc3e3690f4d8db7bc4
p3r7 <11557146+p3r7@users.noreply.github.com>
2021-11-14 21:25:08 +0100

    Fix monobright grid detection

--- lib/grid_utils.lua
+++ lib/grid_utils.lua
@@ -6,7 +6,9 @@ local grid_utils = {}
 -- GRID HW IDENTIFICATION
 
 grid_utils.nb_levels = function(g)
-  if g.name == 'monome 64 m64-0536' then
+  if util.string_starts(g.name, 'monome 64 m64')
+    or util.string_starts(g.name, 'monome 128 m128')
+    or util.string_starts(g.name, 'monome 256 m256') then
     return 1
   else
     return 15
