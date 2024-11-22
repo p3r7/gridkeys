28278557a93760faba4bd36db29d2245b4c1bf26
p3r7 <11557146+p3r7@users.noreply.github.com>
2024-11-22 10:55:36 +0100

    derp derp

--- lib/grid_utils.lua
+++ lib/grid_utils.lua
@@ -3,9 +3,31 @@ local grid_utils = {}
 
 
 -- -------------------------------------------------------------------------
--- GRID HW IDENTIFICATION
+-- valid
 
-grid_utils.nb_levels = function(g)
+function grid_utils.valid(g)
+  return ( g and g.name ~= 'none' )
+end
+
+
+-- -------------------------------------------------------------------------
+-- grid hw identification
+
+function grid_utils.shortname(g)
+  if not g.name then
+    return 'nil'
+  end
+
+  if util.string_starts(g.name, 'monome ') then
+    return (g.name):sub(#'monome ' + 1)
+  elseif util.string_starts(g.name, 'neo-monome ') then
+    return 'neo ' .. (g.name):sub(#'neo-monome ' + 1)
+  else
+    return g.name
+  end
+end
+
+function grid_utils.nb_levels(g)
   if util.string_starts(g.name, 'monome 64 m64')
     or util.string_starts(g.name, 'monome 128 m128')
     or util.string_starts(g.name, 'monome 256 m256') then
